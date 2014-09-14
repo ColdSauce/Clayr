654bdd122c41490976ecfe6e2d64af764acf2745
Chen Ye <blasternt@outlook.com>
2014-09-14 10:21:16 -0400

    stupid stupid

--- app/src/main/java/dwai/clayr/DataLookActivity.java
+++ app/src/main/java/dwai/clayr/DataLookActivity.java
@@ -1,14 +1,17 @@
 package dwai.clayr;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.TypedValue;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -16,6 +19,9 @@ import android.widget.ScrollView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 import dwai.clayr.R;
 
@@ -79,9 +85,62 @@ public class DataLookActivity extends Activity {
 
     public void jsonRead(String json) throws JSONException{
         JSONObject jsonObject = new JSONObject(json);
-
-
-
     }
+
+    public class Range {
+        public String name;
+        public float max;
+        public float min;
+        public Range(String name, float max, float min){
+            this.name = name;
+            this.max = max;
+            this.min = min;
+        }
+    }
+
+    public class Assay {
+        public String name;
+        public ArrayList<Range> ranges;
+        public String brief_desc;
+        public String desc;
+        public String unit;
+        public float value;
+        public Assay(String name, ArrayList<Range> ranges, String brief_desc, String desc, String unit, float value) {
+            this.name = name;
+            this.ranges = ranges;
+            this.brief_desc = brief_desc;
+            this.desc = desc;
+            this.unit = unit;
+            this.value = value;
+        }
+    }
+
+//    public class AssayAdapter extends ArrayAdapter<Assay> {
+//        private ArrayList<Assay> items;
+//        public AssayAdapter(Context context, int textViewResourceId, ArrayList<Assay> items) {
+//            super(context, textViewResourceId, items);
+//            this.items = items;
+//        }
+//
+//        @Override
+//        public View getView(int posiion, View convertView, ViewGroup parent) {
+//            View v = convertView;
+//            if (v == null) {
+////                LayoutInflater vi = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+////                v = vi.inflate(R.layout.row, null);
+//            }
+//            Assay a = items.get(position);
+//            if (a != null) {
+////                TextView tt = (TextView) v.findViewById(R.id.toptext);
+////                TextView bt = (TextView) v.findViewById(R.id.bottomtext);
+////                if (tt != null) {
+////                    tt.setText("Name: "+o.getOrderName());                            }
+////                if(bt != null){
+////                    bt.setText("Status: "+ o.getOrderStatus());
+////                }
+//            }
+//            return v;
+//        }
+//    }
 
 }
